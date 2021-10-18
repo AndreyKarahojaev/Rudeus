24bde33133fcbc20f9bbe52ff63d767e754c34a5
a.karakhodzhaev <karahodschaev@yandex.ru>
2021-10-18 17:31:47 +0400

    problem ошибка правила попутного ветра

--- src/App.vue
+++ src/App.vue
@@ -27,17 +27,17 @@ export default {
 }
 </script>
 
-<style>
-img {
-  display: block;
+<style src="./tailwind.css" />
+
+/* img {
+    display: block;
   margin: 0 auto;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
-}
-</style>
+} */
